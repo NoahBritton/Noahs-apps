0466708d3805f64a266010e527e41c9bed85748a
NoahBritton <noahseslar@gmail.com>
2022-01-21 12:57:28 -0500

    port const

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
+
+const port = process.env.PORT || 300
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
